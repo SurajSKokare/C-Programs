1d1aa3cb0a68b20394b22be7250c38c5104c5695
SurajSKokare <surajkokarek@gmail.com>
2025-10-10 17:45:17 +0530

    Add files via upload

---
+++ Area of Circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int r;
+float area;
+printf("enter the redius of the circle");
+scanf("%d",&r);
+area=3.14*r*r;
+printf("area of the circle=%2f", area);
+getch();
+}
