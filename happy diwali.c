29833cc6d182c1d503268a21e16dfa66bca8d6e2
Surajkokare <surajkokarek@gmail.com>
2025-10-19 07:51:54 +0530

    Happy diwali

---
+++ happy diwali.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+  int n;
+     printf("enter the number  :");
+       scanf("%d",&n);
+        for(int i=1; i<=n; i++){
+        for(int j=1; j<=3; j++){
+     printf("Happy Diwali    ");
+   }
+      printf("\n");
+   }
+   getch();
+}
